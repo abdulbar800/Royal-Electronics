14b519c77ab307bc4a8cd25609b98b9b668d1b93
abdulbar800 <abdulbar800@gmail.com>
2026-08-30 15:09:22 +0530

    Update dashboardController.js

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -1,5 +1,5 @@
-const Order = require('../models/Order');
-const Product = require('../models/Product');
+const Order = require('../models/order');
+const Product = require('../models/product');
 const User = require('../models/User');
 const Coupon = require('../models/Coupon');
 
